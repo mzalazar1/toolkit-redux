d8aa2be20d76a466f09c3795cd66a010d70507b1
mzalazar1 <marcoszalazar86@gmail.com>
2023-03-22 17:14:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { decrement, increment } from './store/slices/counter';
+
+
+function App() {
+
+  const { counter } = useSelector(state => state.counter)
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button type="button" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button type="button" onClick={() => dispatch(incrementBy(2))}>
+          Increment by 2
+        </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
+
