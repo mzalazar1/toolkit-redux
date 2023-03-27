5aa7f4c21e67f7afe0cc9db5a8d35f761b30deb2
mzalazar1 <marcoszalazar86@gmail.com>
2023-03-27 15:08:01 -0300

    descomenté el consolelog

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,7 +1,6 @@
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 
-
 export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
@@ -9,7 +8,7 @@ export const getPokemons = (page = 0) => {
         //TODO: realizar petición http
         const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
         const data = await resp.json();
-        // console.log(data);
+        console.log(data);
 
         dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     }
