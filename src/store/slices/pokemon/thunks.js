54823f609f4677f907b76694f0e470b220a9b171
mzalazar1 <marcoszalazar86@gmail.com>
2023-03-29 00:46:01 -0300

    ultimo commit

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 
@@ -5,10 +6,12 @@ export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
-        //TODO: realizar petición http
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
-        const data = await resp.json();
-        console.log(data);
+        // TODO: realizar petición http
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        // const data = await resp.json();
+        // const resp = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+        // console.log(resp);
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
 
         dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     }
